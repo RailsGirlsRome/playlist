5dbb403929b6bac1401011ca84286fd2f48f2e30
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-16 18:40:09 +0100

    link to playlists from index

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,17 @@
 class PlaylistsController < ApplicationController
 
+  before_filter :find_playlist, :except => :index
+
   def index
     @playlists = Playlist.all
   end
+
+  def show
+  end
+
+  protected
+
+    def find_playlist
+      @playlist = Playlist.find(params[:id])
+    end
 end
