b9259b26e9dd6e94213a9913589517ecc9303680
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-17 16:44:53 +0100

    configure paperclip to store files to Amazon S3

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '3.2.12'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'pg'
-gem 'bootstrap-sass'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -46,3 +45,5 @@ gem "awesome_print"
 
 gem 'devise'
 gem 'paperclip'
+gem 'bootstrap-sass'
+gem 'aws-sdk'
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,12 @@ class Song < ActiveRecord::Base
   attr_accessible :file
 
   belongs_to :playlist
-  has_attached_file :file
+  has_attached_file :file, {
+    :storage        => :s3,
+    :s3_credentials => YAML.load(ERB.new(File.read(Rails.root + 'config/s3.yml.erb')).result(binding)),
+    :bucket         => "rgr13-playlist",
+    :path           => "/npj/:class/:attachment/:id_partition/:style/:filename"
+  }
 
   validates :playlist_id, :presence => true
   validates_attachment :file, :presence => true
