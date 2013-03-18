672aad5f947a6f972398b1dbc774a070dd56f2c2
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-18 21:13:45 +0100

    save songs localy in development, to amazon in production

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,12 +6,16 @@ class Song < ActiveRecord::Base
 
   belongs_to :playlist, :touch => true
 
-  has_attached_file :file, {
-    :storage        => :s3,
-    :s3_credentials => YAML.load(ERB.new(File.read(Rails.root + 'config/s3.yml.erb')).result(binding)),
-    :bucket         => "rgr13-playlist",
-    :path           => "/npj/:class/:attachment/:id_partition/:style/:filename"
-  }
+  if Rails.env.development?
+    has_attached_file :file
+  else
+    has_attached_file :file, {
+      :storage        => :s3,
+      :s3_credentials => YAML.load(ERB.new(File.read(Rails.root + 'config/s3.yml.erb')).result(binding)),
+      :bucket         => "rgr13-playlist",
+      :path           => "/npj/:class/:attachment/:id_partition/:style/:filename"
+    }
+  end
 
   validates :playlist_id, :presence => true
   validates_attachment :file, :presence => true
