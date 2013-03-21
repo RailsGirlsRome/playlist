b18218c8522efcc67c47a3d44a37943637998802
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-21 23:41:13 +0100

    Step 1: Make a Root Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MyPlaylist::Application.routes.draw do
+  resources :playlists
+
+  root :to => "playlists#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
