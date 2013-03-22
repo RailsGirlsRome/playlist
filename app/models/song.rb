bbd1a816902a62ad3a41dc12e70a9ee70a04a42f
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-22 03:08:16 +0100

    Step 12: Deploy

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,8 +5,18 @@ class Song < ActiveRecord::Base
   belongs_to :playlist, :touch => true
 
   validates_presence_of :playlist_id
+  validates_attachment :file
 
-  has_attached_file :file
+  if Rails.env.development?
+    has_attached_file :file
+  else
+    has_attached_file :file, {
+      :storage        => :s3,
+      :s3_credentials => YAML.load(ERB.new(File.read(Rails.root + 'config/s3.yml.erb')).result(binding)),
+      :bucket         => "rgr13-playlist",
+      :path           => "/npj/:class/:attachment/:id_partition/:style/:filename"
+    }
+  end
 
   ranks :row_order, :with_name => :playlist_id
 end
