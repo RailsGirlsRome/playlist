51b6d11573dc1c26fa18a8af9911f3fcf429cdbc
Peter Brindisi <peter.brindisi@gmail.com>
2013-03-22 01:30:54 +0100

    Step 4: Associate Users and Playlists

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+
+  has_one :playlist, :dependent => :destroy
+
+  before_create :build_playlist
 end
